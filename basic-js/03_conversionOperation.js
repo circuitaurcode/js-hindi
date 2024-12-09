2585adcc82e693b2f9ba59766de2ea64bfc631f5
Vivek Prakash <110840403+codebyvivekprakash@users.noreply.github.com>
2024-12-09 13:39:01 +0000

    conversionOpreration

---
+++ basic-js/03_conversionOperation.js
@@ -0,0 +1,41 @@
+let score = 32
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let number = undefined
+
+//console.log(typeof number);
+//console.log(typeof (number));
+
+let valueInNumber = Number(number)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let loggedIn = "vivek"
+
+let booleanLoggedIn = Boolean(loggedIn)
+console.log(booleanLoggedIn);
+
+
+let someNumber = 54
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+
+
+
+
+
+
+
+
