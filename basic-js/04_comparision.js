e54d9879652256007faa859739a0dc10795b6855
Vivek Prakash <110840403+codebyvivekprakash@users.noreply.github.com>
2024-12-09 15:15:34 +0000

    comparision

--- basic-js/04_comparision.js
+++ basic-js/04_comparision.js
@@ -5,8 +5,8 @@
 // console.log(2 != 1);
 
 
-console.log("2" > 1);
-console.log("02" > 1);
+// console.log("2" > 1);
+// console.log("02" > 1);
 
 // console.log(null > 0);
 // console.log(null == 0);
@@ -18,4 +18,10 @@ console.log("02" > 1);
 
 // === 
 
-console.log("2" === 2);
+// console.log("2" === 2);
+
+console.log(2 <3 );
+console.log(2>4);
+console.log(2 < "2");
+
+
