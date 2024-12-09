e5e2a3167761dc23d12503f8faa2652d785e047e
Vivek Prakash <110840403+codebyvivekprakash@users.noreply.github.com>
2024-12-09 13:54:12 +0000

    conversionOperation

--- basic-js/03_conversionOperation.js
+++ basic-js/03_conversionOperation.js
@@ -19,23 +19,38 @@ let valueInNumber = Number(number)
 let loggedIn = "vivek"
 
 let booleanLoggedIn = Boolean(loggedIn)
-console.log(booleanLoggedIn);
+//console.log(booleanLoggedIn);
 
 
 let someNumber = 54
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
 
 // *********************** Operations ***********************
 
 
+let value = 7
+
+let negvalue = -value
+
+//console.log(negvalue);
 
 
 
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
 
 
 
+let str1 = "hello"
 
+let str2 = " vivek"
+let str3 = str1 + str2
+console.log(str3);
